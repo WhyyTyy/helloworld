a45bd09040f6d1814c014c038d0be3b9c938dec7
whyy.tyy <tyronsamuelwork@gmail.com>
2024-03-08 08:27:14 +0200

    [Coded fuction to return app]

--- helloworld.py
+++ helloworld.py
@@ -2,13 +2,9 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route()
-def webhook():
-    if request.method == 'POST':
-        print(request.json)
-        return 'success', 200
-    else:
-        abort(400)
+@app.route("/")
+def helloworld():
+    return "Hi World"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run()
